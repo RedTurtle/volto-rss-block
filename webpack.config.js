52105a13e1339ec39fb986bbe69c63ed67d09af0
Luca Bellenghi <lucabel@gmail.com>
2020-03-12 16:00:32 +0100

    update config files

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var path = require('path')
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    filename: 'volto-rss-block.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'RSSFeedBlock',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
+      },
+      {
+        test: /\.styl$/,
+        loader: 'style-loader!css-loader!stylus-loader',
+      },
+    ],
+  },
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
+    },
+    'prop-types': {
+      commonjs: 'prop-types',
+      commonjs2: 'prop-types',
+      amd: 'prop-types',
+      root: 'PropTypes',
+    },
+  },
+}
